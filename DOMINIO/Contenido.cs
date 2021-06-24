4fdaafe29f484f32b3c3a9fa7e4c497f801065c7
LautaroVillalva <ljvillalva@gmail.com>
2021-06-24 17:56:45 -0300

    Clases agregadas

---
+++ DOMINIO/Contenido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMINIO
+{
+    public class Contenido
+    {
+        public int Id { get; set; }
+        public Clase clase { get; set; }
+        public Ejercicio ejercicio { get; set; }
+        public string Video { get; set; }
+        public int Dia { get; set; }
+    }
+}
--- DOMINIO/Usuario.cs
+++ DOMINIO/Usuario.cs
@@ -12,6 +12,8 @@ namespace DOMINIO
     public class Usuario
     {
         public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Dni { get; set; }
